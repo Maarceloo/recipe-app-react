8f7e6a6fc6ab64d2b873d130f811e74186ef1701
Lucas <joselucasrs@gmail.com>
2022-07-14 18:12:03 -0300

    Adicionando rotas.

---
+++ src/components/Routes.jsx
@@ -0,0 +1,46 @@
+import { Route, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import DoneRecipes from '../pages/DoneRecipes';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+import Login from '../pages/Login';
+import Profile from '../pages/Profile';
+import RecipeDetails from '../pages/RecipeDetails';
+import RecipeInProgress from '../pages/RecipeInProgress';
+import Recipes from '../pages/Recipes';
+
+import Header from './Header';
+import Footer from './Footer';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Route exact path="/">
+      <Login />
+    </Route>
+    <Route path={ ['/foods', '/drinks'] }>
+      <Header />
+      <Recipes />
+      <Footer />
+    </Route>
+    <Route path={ ['/foods/:id', '/drinks/:id'] }>
+      <RecipeDetails />
+    </Route>
+    <Route path={ ['/foods/:id/in-progress', '/drinks/:id/in-progress'] }>
+      <RecipeInProgress />
+    </Route>
+    <Route path="/done-recipes">
+      <Header />
+      <DoneRecipes />
+    </Route>
+    <Route path="/favorite-recipes">
+      <Header />
+      <FavoriteRecipes />
+    </Route>
+    <Route path="/profile">
+      <Header />
+      <Profile />
+      <Footer />
+    </Route>
+  </BrowserRouter>
+);
+
+export default Routes;
