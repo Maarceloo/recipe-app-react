92a9bb75e4a3b7b88587ea1d41d4fc944183306a
Marcelo De Lima <delima_marcelo@outlook.com>
2022-07-15 17:01:57 -0300

    Exercicio 57

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useLocalStorage } from '../assets/hooks';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+function Profile() {
+  const email = useLocalStorage('email', 'get', null);
+
+  return (
+    <div>
+      <h3 data-testid="profile-email">{email}</h3>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ null }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ null }
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ null }
+      >
+        Logout
+
+      </button>
+    </div>
+  );
+}
 
 export default Profile;
