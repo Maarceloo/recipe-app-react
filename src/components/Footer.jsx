9affe77c260c2be9e8eb5ea2b64e6303a5ffefd7
Marcelo De Lima <delima_marcelo@outlook.com>
2022-07-15 19:09:01 -0300

    Desenvolvendo Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
-const Footer = () => (
-  <div>Footer</div>
-);
+function Footer() {
+  const history = useHistory();
+
+  return (
+    <footer data-testid="footer">
+      <button
+        data-testid="drinks-bottom-btn"
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img
+          src={ drinkIcon }
+          alt="drink"
+        />
+      </button>
+      <button
+        data-testid="food-bottom-btn"
+        type="button"
+        onClick={ () => history.push('/foods') }
+      >
+        <img
+          src={ mealIcon }
+          alt="food"
+        />
+      </button>
+    </footer>
+  );
+}
 
 export default Footer;
