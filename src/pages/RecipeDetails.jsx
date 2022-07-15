9dddc2df8b9cf9a6dc52f7183310aceeafa92baf
FaelCaporali <rafaelhon@gmail.com>
2022-07-15 20:13:28 -0300

    requisito 24 concluido e inicio de 25

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useAsyncEffect } from '../assets/hooks';
+import { fetchData } from '../assets/api';
 
-const RecipeDetails = () => (
-  <div>RecipeDetails</div>
-);
+const RecipeDetails = ({ match }) => {
+  const {
+    params: { id },
+    url,
+  } = match;
+  const [recipe, setRecipe] = useState('');
+  const recipeType = url.includes('foods') ? 'Meal' : 'Drink';
+
+  useAsyncEffect(async () => {
+    await fetchData
+      .detail({ recipeType, id })
+      .then((info) => setRecipe(info[0]));
+  }, [id, url]);
+  return (
+    <section>
+      <img
+        src={ recipe[`str${recipeType}Thumb`] }
+        alt={ recipe[`str${recipeType}`] }
+        data-testid="recipe-photo"
+      />
+      <h2>{recipe[`str${recipeType}`]}</h2>
+      {/* <p>{recipeType === 'Meal' ? }</p> */}
+
+      <p>{id}</p>
+      {recipe !== '' && <p>{recipe[`str${recipeType}`]}</p>}
+    </section>
+  );
+};
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default RecipeDetails;
