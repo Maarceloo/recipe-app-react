ab5fd94880a937319a348962487137c4116eb1d8
FaelCaporali <rafaelhon@gmail.com>
2022-07-15 17:58:59 -0300

    pegando props de detailsPage

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -16,13 +16,13 @@ const Routes = () => (
     <Route exact path="/">
       <Login />
     </Route>
-    <Route path={ ['/foods', '/drinks'] }>
+    <Route exact path={ ['/foods', '/drinks'] }>
       <Header />
       <Recipes />
       <Footer />
     </Route>
-    <Route path={ ['/foods/:id', '/drinks/:id'] }>
-      <RecipeDetails />
+    <Route exact path={ ['/foods/:id', '/drinks/:id'] }>
+      {(props) => <RecipeDetails { ...props } />}
     </Route>
     <Route path={ ['/foods/:id/in-progress', '/drinks/:id/in-progress'] }>
       <RecipeInProgress />
