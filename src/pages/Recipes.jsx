45742a0e3b685bb4e86063a83075df227315d16c
Lucas <joselucasrs@gmail.com>
2022-07-15 20:28:22 -0300

    Pegando lista de receitas iniciais.

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AppContext from '../context';
+import useRecipeType from '../assets/hooks/useRecipeType';
 
-const Recipes = () => (
-  <div>Recipes</div>
-);
+const Recipes = () => {
+  const { updateFilters } = useContext(AppContext);
+  const recipeType = useRecipeType();
+
+  useEffect(() => {
+    updateFilters({ searchOption: 'byName', recipeType, queryText: '' });
+  }, []);
+
+  return (
+    <div>Recipes</div>
+  );
+};
 
 export default Recipes;
