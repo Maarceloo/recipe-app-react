3e6b9e381e6d45d5d4e828d53b3b04558afb0902
FaelCaporali <rafaelhon@gmail.com>
2022-07-18 21:55:55 -0300

    absorvendo código do Marcelo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
-const Footer = () => (
-  <div>Footer</div>
-);
+function Footer() {
+  const history = useHistory();
+
+  return (
+    <footer data-testid="footer">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drink"
+        />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/foods') }
+      >
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="food"
+        />
+      </button>
+    </footer>
+  );
+}
 
 export default Footer;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { changeLocalStorage } from '../assets/hooks';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+function Profile() {
+  const history = useHistory();
+
+  const user = changeLocalStorage('user');
+
+  const handleClickLogout = () => {
+    history.push('/');
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      { user && (<h3 data-testid="profile-email">{ user.email }</h3>)}
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ handleClickLogout }
+      >
+        Logout
+
+      </button>
+    </div>
+  );
+}
 
 export default Profile;
