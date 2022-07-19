381bcd8b38389a56ccf35c1cda74d59b72ac7ce3
FaelCaporali <rafaelhon@gmail.com>
2022-07-18 21:47:07 -0300

    resolvendo conflitos

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Login from './pages/Login';
 
 test('Farewell, front-end', () => {
-  render(<App />);
+  render(<Login />);
   const linkElement = screen.getByText(/TRYBE/i);
   expect(linkElement).toBeInTheDocument();
 });
