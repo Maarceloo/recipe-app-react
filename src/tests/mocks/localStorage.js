03b1908c3aacafddc1120fe281944bc1b36cd189
FaelCaporali <rafaelhon@gmail.com>
2022-07-20 00:15:25 -0300

    mais uma referencia de localStorageMock

--- src/tests/mocks/localStorage.js
+++ src/tests/mocks/localStorage.js
@@ -59,4 +59,76 @@ expect(localStorage.setItem).toHaveBeenCalled();
 // resposta ao comentário
 // Actually it works for me just with the spyOn, no need to override setItem function jest.spyOn(window.localStorage.__proto__, 'setItem');
 
+// 4.
+// Here is a simple local storage fake
+const fakeLocalStorage = (function() {
+  let store = {};
 
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem: function(key) {
+      delete store[key];
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+})();
+
+// Wiring
+// localStorage is a read-only property of the window interface, so it is not possible to just reassign it like window.localStorage = fakeLocalStorage
+Object.defineProperty(window, 'localStorage', {
+  value: fakeLocalStorage
+});
+
+// Full working example
+// Simple function that uses the localStorage API
+
+// storage.js
+
+export function saveToStorage(value) {
+  window.localStorage.setItem('the-key', value);
+}
+
+// Corresponding jest test
+// storage.test.js
+
+import { saveToStorage } from './storage';
+
+const fakeLocalStorage = (function () {
+  let store = {};
+
+  return {
+    getItem: function (key) {
+      return store[key] || null;
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString();
+    },
+    removeItem: function (key) {
+      delete store[key];
+    },
+    clear: function () {
+      store = {};
+    }
+  };
+})();
+
+describe('storage', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: fakeLocalStorage,
+    });
+  });
+
+  it('saves the key to the storage', () => {
+    saveToStorage('fake-value');
+
+    expect(window.localStorage.getItem('the-key')).toEqual('fake-value');
+  });
+});
